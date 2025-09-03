0c1cae54eaaeb9f39eb6ee4a41b0c06dc489a83f
Dennis Yaremenko <yaremdennis@gmail.com>
2025-09-03 11:14:13 +0300

    Add text, image, scaffold, material app

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,42 @@ import 'package:flutter/cupertino.dart';
 void main() {
   // runApp(Text('hello world', textDirection: TextDirection.ltr),);
 
- // runApp(Center(child: Text('hello world', textDirection: TextDirection.ltr)));
+  // runApp(Center(child: Text('hello world', textDirection: TextDirection.ltr)));
 
-//  runApp(CupertinoApp());
+  //  runApp(CupertinoApp());
 
- runApp(MaterialApp(
-  home: 
-  Center(child: Text('hello world', textDirection: TextDirection.ltr))
- ));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Home page'),
+          actions: [
 
+          ],
+        ),
+        body: Center(
+          child:
+         // Image.network('src')
+
+           Icon(
+            Icons.accessibility_new_sharp,
+            fontWeight: FontWeight.bold,
+            size: 50,
+            color: Colors.blueAccent,
+           
+            ),
+
+          //  Text(
+          //   'hello world', 
+          //   textDirection: TextDirection.ltr,
+          //   style: TextStyle(
+          //     fontSize: 25,
+          //     fontWeight: FontWeight.bold,
+          //   ),
+            // ),
+        ),
+      ),
+    ),
+  );
 }
