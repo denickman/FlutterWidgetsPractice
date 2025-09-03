fb9787f1b55ffcb0c985b3629a56be02502d9ae3
Dennis Yaremenko <yaremdennis@gmail.com>
2025-09-03 10:45:27 +0300

    Initiall commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  // runApp(Text('hello world', textDirection: TextDirection.ltr),);
+
+ // runApp(Center(child: Text('hello world', textDirection: TextDirection.ltr)));
+
+//  runApp(CupertinoApp());
+
+ runApp(MaterialApp(
+  home: 
+  Center(child: Text('hello world', textDirection: TextDirection.ltr))
+ ));
+
+}
