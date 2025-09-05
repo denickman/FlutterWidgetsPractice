6a779909315a36815335517bdc82364e6b54604c
Dennis Yaremenko <yaremdennis@gmail.com>
2025-09-05 15:37:32 +0300

    Update input widgets

--- lib/input_widget.dart
+++ lib/input_widget.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class InputWidget extends StatefulWidget {
@@ -10,14 +8,54 @@ class InputWidget extends StatefulWidget {
 }
 
 class _InputWidgetState extends State<InputWidget> {
+  final _nameController = TextEditingController();
+  final countries = ["Ukroine", 'Australia', 'UK', 'USA', 'Canada', 'Denmark'];
+  String? country;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Input widgets'),
-      ),
+      appBar: AppBar(title: Text('Input widgets')),
       body: Center(
-        
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: 'Enter your name',
+                label: Text('Enter your name'),
+              ),
+              onChanged: (value) {
+                print('text changed: $value');
+              },
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                print(_nameController.text);
+              },
+              child: Text('Ok'),
+            ),
+
+            Divider(height: 2, thickness: 2),
+
+            DropdownButton<String>(
+              isExpanded: true,
+              hint: Text('Select your country'),
+              value: country,
+              items: countries.map(
+                (country) => DropdownMenuItem<String>(
+                  value: country,
+                  child: Text(country),
+                  )
+                ).toList(),
+               onChanged: (value) {
+                    setState(() {
+                      country = value;
+                    });
+              }),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:widgets/profile_page.dart';
 import 'package:widgets/list_view_example.dart';
 import 'package:widgets/grid_view_example.dart';
 import 'package:widgets/mixed_view_example.dart';
-import 'package:widgets/menu_page.dart';
 import 'package:widgets/counter_app.dart';
 import 'package:widgets/home_page.dart';
 import 'package:widgets/input_widget.dart';
@@ -21,6 +20,17 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
+  final _pages = [
+    {'title': 'Open Simple Widget', 'widget': SimpleWidget()},
+    {'title': 'Open Profile Assignment', 'widget': ProfileAssignment()},
+    {'title': 'Open List View Example', 'widget': ListViewExample()},
+    {'title': 'Open Grid View Example', 'widget': GridViewExample()},
+    {'title': 'Open Menu Page', 'widget': MenuPage()},
+    {'title': 'Open Counter App', 'widget': CounterApp()},
+    {'title': 'Open Home Page', 'widget': HomePage()},
+    {'title': 'Open Input Widget', 'widget': InputWidget()},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,114 +43,27 @@ class HomeScreen extends StatelessWidget {
             style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
           ),
         ),
-        actions: [],
       ),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SimpleWidget()),
-                  );
-                },
-                child: Text('Open Simple Widget'),
-              ),
-
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ProfileAssignment(),
-                    ),
-                  );
-                },
-                child: Text('Open Profile Assignment'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ListViewExample()),
-                  );
-                },
-                child: Text('Open List View Example'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => GridViewExample()),
-                  );
-                },
-                child: Text('Open Grid View Example'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MenuPage()),
-                  );
-                },
-                child: Text('Open Menu Page'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CounterApp()),
-                  );
-                },
-                child: Text('Open Counter App'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-                child: Text('Open Home Page'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => InputWidget()),
-                  );
-                },
-                child: Text('Open Input Widget'),
-              ),
-            ],
+            children: _pages.map((page) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => page['widget'] as Widget),
+                    );
+                  },
+                  child: Text(page['title'] as String),
+                ),
+              );
+            }).toList(),
           ),
         ),
       ),
     );
   }
 }
-
-/*
-
-MainAxisAlignment.start → все элементы прижаты к верху.
-MainAxisAlignment.end → все элементы прижаты к низу.
-MainAxisAlignment.center → элементы собраны по центру.
-MainAxisAlignment.spaceBetween → первый элемент вверху, последний внизу, остальные равномерно распределены между ними без отступов сверху/снизу.
-MainAxisAlignment.spaceAround → между элементами равные промежутки, и половина этих промежутков добавляется сверху и снизу.
-MainAxisAlignment.spaceEvenly → все промежутки (между элементами, сверху и снизу) одинаковые.
-
-
-Stack(
-  clipBehavior: Clip.hardEdge, // по умолчанию
-  children: [...],
-)
-clipBehavior может быть:
-Clip.none — не обрезать. Всё, что выходит за границы Stack, будет видно.
-Clip.hardEdge — обрезать строго по границе Stack. Всё, что выходит за границы, не отображается.
-Clip.antiAlias — обрезка с сглаживанием краёв (антиалиасинг).
-Clip.antiAliasWithSaveLayer — как antiAlias, но создаёт отдельный слой (дороже по производительности).
-
-*/
